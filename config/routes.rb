af8eb54f34763f29ea38229dd689c440b53431f9
Elizabeth Jacobs <elizabethjjacobs@gmail.com>
2016-12-26 14:32:37 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :songs
+  resources :genres
+  resources :artists
+
+  # get '/songs/:id', to: 'songs#show', as: 'navigate'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
